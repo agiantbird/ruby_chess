5b12ee8f864392e6160a384c99a77ad1fac6265f
Rich Hoyt <rhoyt10@gmail.com>
2022-04-17 11:15:45 -0400

    Add board renderer class

---
+++ lib/board_renderer_text.rb
@@ -0,0 +1,16 @@
+class BoardRendererText
+	attr_reader :board
+	def initialize(board)
+		@board = board
+	end
+
+	def render
+		8.times do |r|
+			puts "------------"
+			8.times do |c|
+				print board[[r, c]]
+			end
+			puts ""
+		end
+	end
+end
--- main.rb
+++ main.rb
@@ -1,5 +1,10 @@
+require_relative './lib/bishop.rb'
 require_relative './lib/board.rb'
+require_relative './lib/board_renderer_text.rb'
+require_relative './lib/king.rb'
+require_relative './lib/knight.rb'
 require_relative './lib/pawn.rb'
+require_relative './lib/queen.rb'
 require_relative './lib/rook.rb'
 
 # b = Board.new
@@ -14,16 +19,17 @@ require_relative './lib/rook.rb'
 # puts b.in_bounds?([10, 0])
 
 b = Board.new
-b[[1,0]] = Pawn.new([1, 0], :black)
-b[[1,1]] = Pawn.new([1, 0], :black)
-b[[1,2]] = Pawn.new([1, 0], :white)
-b[[1,3]] = Pawn.new([1, 0], :black)
-b[[1,4]] = Pawn.new([1, 0], :black)
-b[[1,5]] = Pawn.new([1, 0], :black)
-b[[1,6]] = Pawn.new([1, 0], :black)
-b[[1,7]] = Pawn.new([1, 0], :black)
+b[[1,0]] = Pawn.new(:black)
+b[[1,1]] = Pawn.new(:black)
+b[[1,2]] = Pawn.new(:white)
+b[[1,3]] = Pawn.new(:black)
+b[[1,4]] = Pawn.new(:black)
+b[[1,5]] = Pawn.new(:black)
+b[[1,6]] = Pawn.new(:black)
+b[[1,7]] = Pawn.new(:black)
 
-puts b.grid
+text_board = BoardRendererText.new(b)
+puts text_board.render
 # pawn = Pawn.new([1, 0], :black)
 # puts pawn
 
