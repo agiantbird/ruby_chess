7b14f977769774f97d449a7af3bd6ea659cd87de
Rich Hoyt <rhoyt10@gmail.com>
2022-04-20 10:56:54 -0400

    Add available moves for Pawn class

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,14 +1,51 @@
 class Pawn < Piece
-	#include Stepable
+	
 	def forward_dir
-		color == :black ? -1 : 1
+		color == :black ? 1 : -1
+	end
+
+	def at_start?
+		(color == :black && current_row == 1) ||
+			(color == :white && current_row == 6)
+	end
+
+	def available_moves
+		moves = []
+		one_forward = [current_row + forward_dir, current_column]
+
+		# can move forward one
+		if board.empty?(one_forward)
+			moves << one_forward
+		end
+
+		# if first move, can move two spaces
+		two_forward = [current_row + (2 * forward_dir), current_column]
+		if board.empty?(one_forward) && board.empty?(two_forward) && at_start?
+			moves << two_forward
+		end
+
+		# if enemy on diagonal, can move diagonal 1 into that spot
+		# diag left
+		diag_left = [current_row + forward_dir, current_column + 1]
+		# diag right
+		diag_right = [current_row + forward_dir, current_column + -1]
+
+		if enemy?(diag_left)
+			moves << diag_left
+		end
+
+		if enemy?(diag_right)
+			moves << diag_right
+		end
+
+		moves.select {|m| board.in_bounds?(m)}
 	end
 
 	def to_s
 		color == :black ? "♟" : "♙"
 	end
 
-	def move_dirs
-		[[0, 1]]
-	end
+	# def move_dirs
+	# 	[[0, 1]]
+	# end
 end
--- main.rb
+++ main.rb
@@ -3,9 +3,15 @@ require_relative './lib/board_renderer_text.rb'
 require_relative './lib/board.rb'
 
 b = Board.start_chess
-
+# add pieces with PieceName.new(board, :color, loc)
 text_board = BoardRendererText.new(b)
+# puts text_board.render
+
+b[[2, 2]] = Pawn.new(b, :white, [2,2])
+b[[2, 0]] = Pawn.new(b, :white, [2,0])
+p b[[1,2]].available_moves
 puts text_board.render
+
 
 # b = Board.new
 # b[[0, 2]] = King.new(b, :black, [2, 2])
